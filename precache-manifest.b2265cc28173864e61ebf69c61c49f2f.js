4da29d6c19ec88c5bd9fe8723d1eab7537c9ef1f
Marcus Mimnagh <marcusmimnagh@Marcuss-MacBook-Pro.local>
2020-05-05 18:41:19 -0500

    first commit

---
+++ precache-manifest.b2265cc28173864e61ebf69c61c49f2f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e186f9c6d347f14549c672f1f6abe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b6c78421ea55dc1b5b5",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "58c9e73dd2475ec334a8",
+    "url": "/static/css/main.965b6121.chunk.css"
+  },
+  {
+    "revision": "2b6c78421ea55dc1b5b5",
+    "url": "/static/js/2.fa32a0fe.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.fa32a0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c9e73dd2475ec334a8",
+    "url": "/static/js/main.ddc21d89.chunk.js"
+  },
+  {
+    "revision": "1b70ad4dc61068256018",
+    "url": "/static/js/runtime-main.e34632f5.js"
+  },
+  {
+    "revision": "71dd7432d7b15adcc5feeaeb64c05ab7",
+    "url": "/static/media/Azonix.71dd7432.woff"
+  },
+  {
+    "revision": "727139a7ab86109b9a654882a3b3982c",
+    "url": "/static/media/face.727139a7.png"
+  }
+]);
